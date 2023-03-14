4c3de16e083a78e92c06d5bce432538721344b5c
Chris1320 <chris1320is@protonmail.com>
2023-03-14 15:37:32 +0800

    Added NetBeans project.
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

---
+++ InventorySystem/src/main/java/com/group1/inventorysystem/Info.java
@@ -0,0 +1,10 @@
+package com.group1.inventorysystem;
+
+/**
+ * This class includes all general information about the software.
+ * 
+ * @author Chris
+ */
+public class Info {
+    public static String NAME = "Inventory System";
+}
---
+++ InventorySystem/src/main/java/com/group1/inventorysystem/InventorySystem.java
@@ -0,0 +1,23 @@
+package com.group1.inventorysystem;
+
+import javax.swing.JFrame;
+
+/**
+ * This is the class the includes the main method.
+ * 
+ * @author Chris
+ */
+public class InventorySystem {
+    /**
+     * This is the main method.
+     * @param args The command-line arguments.
+     */
+    public static void main(String[] args) {
+        JFrame main_window = new JFrame(Info.NAME);
+        main_window.setContentPane(new Main(main_window).getPanel());
+        main_window.setResizable(false);
+        main_window.pack();
+        main_window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        main_window.setVisible(true);
+    }
+}
