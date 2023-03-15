4f7b8c34b89f6d377f285125696fb17bc878002b
chris1320 <chris1320is@protonmail.com>
2023-03-15 09:28:48 +0800

    Updated MySQL Configuration.
    Signed-off-by: chris1320 <chris1320is@protonmail.com>

--- InventorySystem/src/main/java/com/group1/inventorysystem/Info.java
+++ InventorySystem/src/main/java/com/group1/inventorysystem/Info.java
@@ -2,25 +2,26 @@ package com.group1.inventorysystem;
 
 /**
  * This class includes all general information about the software.
- * 
+ *
  * @author Chris
  */
 public class Info {
+
     public static String NAME = "Inventory System";
 
     // if true, do not connect to the database. ! FOR DEV ENV ONLY !
     final public static boolean OFFLINE_MODE = false;
 
     // Database information
-    final public static String DB_SERVER_HOST = "localhost";
-    final public static int DB_SERVER_PORT = 3306;
+    final public static String DB_SERVER_HOST = "0.tcp.ap.ngrok.io";
+    final public static int DB_SERVER_PORT = 18171;
     final public static String[] DB_CREDENTIALS = {"root", ""};
     final public static String DB_NAME = "InventorySystem";
     final public static String DB_SERVER_URL = String.format(
-        "jdbc:mysql://%s:%s/%s",
-        DB_SERVER_HOST,
-        DB_SERVER_PORT,
-        DB_NAME
+            "jdbc:mysql://localhost/%s?user=%s&password=%s&useUnicode=true&characterEncoding=UTF-8",
+            DB_NAME,
+            DB_CREDENTIALS[0],
+            DB_CREDENTIALS[1]
     );
 
 }
