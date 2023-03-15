0650fcd17d0742801805e5d0c09f6fbf806dbe14
Chris1320 <chris1320is@protonmail.com>
2023-03-16 00:26:44 +0800

    Updated `EmployeeAdd.java`.
    - Added placeholder text in first, middle, and last names when the text
      fields hasn't been in focus yet.
    
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

--- InventorySystem/src/main/java/com/group1/inventorysystem/EmployeeAdd.java
+++ InventorySystem/src/main/java/com/group1/inventorysystem/EmployeeAdd.java
@@ -1,7 +1,6 @@
 package com.group1.inventorysystem;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import javax.swing.JFrame;
@@ -9,19 +8,30 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 /**
- *
  * @author jhazminereigne
  */
 public class EmployeeAdd extends javax.swing.JPanel {
-
+    AssetManager asset_manager = new AssetManager();
     JFrame main_frame;
+    
+    boolean fnametxt_modified = false;
+    boolean mnametxt_modified = false;
+    boolean lnametxt_modified = false;
 
     public EmployeeAdd(JFrame main_frame) {
         initComponents();
         this.main_frame = main_frame;
+        
+        // Show placeholder text in name.
+        fnametxt.setText("First Name");
+        mnametxt.setText("Middle Name");
+        lnametxt.setText("Last Name");
+        fnametxt.setForeground(ColorManager.PLACEHOLDER_FG);
+        mnametxt.setForeground(ColorManager.PLACEHOLDER_FG);
+        lnametxt.setForeground(ColorManager.PLACEHOLDER_FG);
     }
 
-    public void Clear() {
+    public void clear() {
         empidtxt.setText("");
         fnametxt.setText("");
         mnametxt.setText("");
@@ -64,6 +74,7 @@ public class EmployeeAdd extends javax.swing.JPanel {
         lnametxt = new javax.swing.JTextField();
         passtxt = new javax.swing.JPasswordField();
         confirmpasstxt = new javax.swing.JPasswordField();
+        jLabel1 = new javax.swing.JLabel();
 
         jScrollPane1.setViewportView(jEditorPane1);
 
@@ -77,6 +88,15 @@ public class EmployeeAdd extends javax.swing.JPanel {
 
         password.setText("Password");
 
+        fnametxt.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                fnametxtFocusGained(evt);
+            }
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                fnametxtFocusLost(evt);
+            }
+        });
+
         usernametxt.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 usernametxtActionPerformed(evt);
@@ -85,7 +105,7 @@ public class EmployeeAdd extends javax.swing.JPanel {
 
         confirmpassword.setText("Confirm Password");
 
-        add.setText("ADD");
+        add.setText("ADD EMPLOYEE");
         add.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 addActionPerformed(evt);
@@ -98,6 +118,28 @@ public class EmployeeAdd extends javax.swing.JPanel {
                 backActionPerformed(evt);
             }
         });
+
+        mnametxt.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                mnametxtFocusGained(evt);
+            }
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                mnametxtFocusLost(evt);
+            }
+        });
+
+        lnametxt.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                lnametxtFocusGained(evt);
+            }
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                lnametxtFocusLost(evt);
+            }
+        });
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
+        jLabel1.setIcon(asset_manager.getImageIcon("inventory.png", 50, 50));
+        jLabel1.setText("Inventory System ");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -141,11 +183,17 @@ public class EmployeeAdd extends javax.swing.JPanel {
                             .addComponent(confirmpasstxt, javax.swing.GroupLayout.PREFERRED_SIZE, 352, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(deptxt, javax.swing.GroupLayout.PREFERRED_SIZE, 352, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addContainerGap(54, Short.MAX_VALUE))))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(130, 130, 130)
+                .addComponent(jLabel1)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(66, 66, 66)
+                .addGap(23, 23, 23)
+                .addComponent(jLabel1)
+                .addGap(34, 34, 34)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(empidtxt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(employeeid))
@@ -159,7 +207,7 @@ public class EmployeeAdd extends javax.swing.JPanel {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(usernametxt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(username))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 21, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jLabel3)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -173,7 +221,7 @@ public class EmployeeAdd extends javax.swing.JPanel {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(department)
                     .addComponent(deptxt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 60, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 36, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(add)
                     .addComponent(back))
@@ -203,14 +251,13 @@ public class EmployeeAdd extends javax.swing.JPanel {
             Connection con = SQLHandler.getConnection();
             Statement statement = con.createStatement();
 
+            // FIXME: SQL Injection vulnerability.
             statement.executeUpdate("INSERT INTO employees VALUES('" + employeeID + "', '" + firstname + "', '" + middlename + "','"
                     + lastname + "','" + username + "', '" + password + "','" + department + "');");
-            JOptionPane.showMessageDialog(null, "Employee added successfully!!");
-            Clear();
-
+            JOptionPane.showMessageDialog(main_frame, "Employee added successfully!!");
+            clear();
         } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null, e);
-
+            JOptionPane.showMessageDialog(main_frame, "Error: " + e);
         }
     }//GEN-LAST:event_addActionPerformed
 
@@ -219,6 +266,39 @@ public class EmployeeAdd extends javax.swing.JPanel {
         main_frame.pack();
         main_frame.validate();
     }//GEN-LAST:event_backActionPerformed
+
+    private void fnametxtFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_fnametxtFocusGained
+        if (!fnametxt_modified) {
+            fnametxt.setForeground(ColorManager.TEXTFIELD_FG);
+            fnametxt.setText("");
+            fnametxt_modified = true;
+        }
+    }//GEN-LAST:event_fnametxtFocusGained
+
+    private void fnametxtFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_fnametxtFocusLost
+    }//GEN-LAST:event_fnametxtFocusLost
+
+    private void mnametxtFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_mnametxtFocusGained
+        if (!mnametxt_modified) {
+            mnametxt.setForeground(ColorManager.TEXTFIELD_FG);
+            mnametxt.setText("");
+            mnametxt_modified = true;
+        }
+    }//GEN-LAST:event_mnametxtFocusGained
+
+    private void mnametxtFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_mnametxtFocusLost
+    }//GEN-LAST:event_mnametxtFocusLost
+
+    private void lnametxtFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_lnametxtFocusGained
+        if (!lnametxt_modified) {
+            lnametxt.setForeground(ColorManager.TEXTFIELD_FG);
+            lnametxt.setText("");
+            lnametxt_modified = true;
+        }
+    }//GEN-LAST:event_lnametxtFocusGained
+
+    private void lnametxtFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_lnametxtFocusLost
+    }//GEN-LAST:event_lnametxtFocusLost
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -233,6 +313,7 @@ public class EmployeeAdd extends javax.swing.JPanel {
     private javax.swing.JLabel employeename;
     private javax.swing.JTextField fnametxt;
     private javax.swing.JEditorPane jEditorPane1;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextField lnametxt;
