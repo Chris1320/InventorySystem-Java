13bdb36d1c887b7fb5599e106d3379dccfee45c5
Chris1320 <chris1320is@protonmail.com>
2023-03-16 00:23:26 +0800

    Added Color Manager module.
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

---
+++ InventorySystem/src/main/java/com/group1/inventorysystem/ColorManager.java
@@ -0,0 +1,13 @@
+package com.group1.inventorysystem;
+
+import java.awt.Color;
+
+/**
+ * This stores all Color objects that is used in the program.
+ * 
+ * @author Chris
+ */
+public class ColorManager {
+    public static Color TEXTFIELD_FG = new Color(0, 0, 0);
+    public static Color PLACEHOLDER_FG = new Color(160, 160, 160);
+}
